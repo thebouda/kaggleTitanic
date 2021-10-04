8331f3e18ac432ef1b5c847edb783759d773d182
baracouda <a.baldo.upc@gmail.com>
2021-10-04 16:07:27 +0200

    first

---
+++ getData.py
@@ -0,0 +1,17 @@
+#%%
+"""
+Creation of code 3/10
+author: Alfredo 
+"""
+###
+import pandas as pd
+#%%
+path = "Data/"
+testFiles = open(path + "test.csv")
+trainFiles= open(path + "train.csv")
+genderSubmissionFiles =  open(path + "gender_submission.csv")
+
+#%%
+
+###
+
